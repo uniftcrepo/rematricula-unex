d61ba23fa9854823d257ef4e7e8e555c09448ff0
jose-guilherme-ti <jose.guilherme.ti@gmail.com>
2023-07-14 12:16:56 -0300

    Ajuste GTM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-2398475-47');
+ReactGA.initialize('GTM-MJR9QHL');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 console.disableYellowBox = true
